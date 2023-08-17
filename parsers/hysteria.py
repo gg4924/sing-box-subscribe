96eeae17aac88f72df6443282fbb01dc4e0fce69
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-08-17 15:03:14 +0800

    修复hysteria错误

--- parsers/hysteria.py
+++ parsers/hysteria.py
@@ -13,8 +13,8 @@ def parse(data):
         'type': 'hysteria',
         'server': re.sub(r"\[|\]", "", server_info.netloc.rsplit(":", 1)[0]),
         'server_port': int(server_info.netloc.rsplit(":", 1)[1]),
-        'up_mbps':netquery.get('upmbps'),
-        'down_mbps': netquery.get('downmbps'),
+        'up_mbps':int(netquery.get('upmbps')),
+        'down_mbps': int(netquery.get('downmbps')),
         'auth_str': netquery.get('auth'),
         'tls': {
             'enabled': True,
