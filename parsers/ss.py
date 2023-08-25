c88d9bed35459c3369f62ea784b104b677017bb4
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-08-25 11:19:04 +0800

    add shadow-tls plugin

--- parsers/ss.py
+++ parsers/ss.py
@@ -1,4 +1,3 @@
-import tool,json,re,urllib,sys
 def parse(data):
     param = data[5:]
     if not param or param.isspace():
@@ -12,21 +11,16 @@ def parse(data):
         'password':None
     }
     if param.find('#') > -1:
+        flag = 0
         remark = urllib.parse.unquote(param[param.find('#') + 1:])
         node['tag'] = tool.rename(remark)
         param = param[:param.find('#')]
     if param.find('/?') > -1:
-        plugin = urllib.parse.unquote(param[param.find('/?') + 2:])
-        if plugin.startswith('plugin'):
-            p1 = 0
-            node['plugin'] = plugin.split(';',1)[0].split('=')[1]
-        if plugin.startswith('v2ray-plugin'):
-            p1 =1
-            node['plugin'] = 'v2ray-plugin'
-            plugin = str(tool.b64Decode(plugin.split('=')[1]),'utf-8')
         plugin_opts={}
+        plugin = urllib.parse.unquote(param[param.find('/?') + 2:])
         param = param[:param.find('/?')]
-        if p1 == 0 :
+        if plugin.startswith('plugin'):
+            node['plugin'] = plugin.split(';',1)[0].split('=')[1]
             for p in plugin.split(';'):
                 key_value = p.split('=')
                 kname = key_value[0]
@@ -34,14 +28,43 @@ def parse(data):
                 if kname in pdict.keys():
                     #kname = pdict[kname]
                     plugin_opts[kname] = key_value[1]
-        if p1 == 1 :
+        node['plugin_opts']=re.sub(r"\{|\}|\"|\\|\:|\&|\s+", "", json.dumps(plugin_opts).replace(':','=', 2).replace(',',';').replace('Host','').replace('group',''))
+    if param.find('?') > -1 and param[param.find('?')-1] != '/':
+        plugin_opts={}
+        plugin = urllib.parse.unquote(param[param.find('?') + 1:])
+        param = param[:param.find('?')]
+        if plugin.startswith('v2ray-plugin'):
+            node['plugin'] = 'v2ray-plugin'
+            plugin = str(tool.b64Decode(plugin.split('=')[1]),'utf-8')
             plugin = eval(plugin.replace('true','1'))
             for kname in plugin.keys():
                 pdict = {'mode':'obfs','host':'obfs-host'}
                 if kname in pdict.keys():
                     #kname = pdict[kname]
                     plugin_opts[kname] = plugin[kname]
-        node['plugin_opts']=re.sub(r"\{|\}|\"|\\|\:|\&|\s+", "", json.dumps(plugin_opts).replace(':','=', 2).replace(',',';').replace('Host','').replace('group',''))
+            node['plugin_opts']=re.sub(r"\{|\}|\"|\\|\:|\&|\s+", "", json.dumps(plugin_opts).replace(':','=', 2).replace(',',';'))
+        elif plugin.startswith('shadow-tls'):
+            flag = 1
+            plugin = eval(str(tool.b64Decode(plugin.split('=')[1]),'utf-8'))
+            node['detour'] = tool.genName()
+            node_tls = {
+                'tag':node['detour'],
+                'type':'shadowtls',
+                'server':plugin['address'],
+                'server_port':int(plugin['port']),
+                'version':int(plugin.get('version', '1')),
+                'password':plugin.get('password', '')
+            }
+            if plugin.get('host'):
+                node_tls['tls']={
+                    'enabled': True,
+                    'server_name': plugin.get('host')
+                }
+                if plugin.get('fp'):
+                    node_tls['tls']['utls']={
+                        'enabled': True,
+                        'fingerprint': plugin.get('fp')
+                    }
     if param.find('@') > -1:
         matcher = re.match(r'(.*?)@(.*):(.*)', param)
         if matcher:
@@ -66,4 +89,7 @@ def parse(data):
         else:
             return None
     node['server_port'] = int(node['server_port'])
-    return node
+    if flag:
+        return (node,node_tls)
+    else:
+        return node
