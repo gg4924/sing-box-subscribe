935b6b3d1b788cf8b05fd363919fda46db727d45
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-09-23 15:18:31 +0800

    fix err, add clash_tuic parser

--- parsers/clash2base64.py
+++ parsers/clash2base64.py
@@ -11,12 +11,21 @@ def clash2v2ray(share_link):
             "port": share_link['port'],
             "id": share_link['uuid'],
             "aid": share_link['alterId'],
-            "net": share_link['network'],
+            "net": share_link.get('network', 'tcp'),
             "type": "none",
             "host": share_link.get('ws-opts', {}).get('headers', {}).get('Host') or share_link.get('ws-headers', {}).get('Host', ''),
             "path": share_link.get('ws-path', {}) or share_link.get('ws-opts', {}).get('path'),
-            "tls": ""
+            "tls": ''
         }
+        if share_link.get('tls') != False:
+            vmess_info['tls'] = 'tls'
+        if vmess_info['net'] == 'grpc':
+            vmess_info["type"] = share_link.get('grpc-opts', {}).get('grpc-mode')
+            vmess_info["sni"] = share_link.get('servername', '')
+            if share_link.get('grpc-opts', {}).get('grpc-service-name') != '/':
+                vmess_info["path"] = share_link.get('grpc-opts', {}).get('grpc-service-name')
+            else:
+                vmess_info["path"] = ''
         vmess_json = json.dumps(vmess_info).encode('utf-8')
         vmess_base64 = base64.b64encode(vmess_json).decode('utf-8')
         link = f"vmess://{vmess_base64}"
@@ -56,15 +65,34 @@ def clash2v2ray(share_link):
         link = f"ssr://{base_link}"
         # TODO
     elif share_link['type'] == 'trojan':
-        link = "trojan://{password}@{server}:{port}?allowInsecure={allowInsecure}&peer={sni}sni={sni}{skip_cert_verify}#{name}".format(
-        password = share_link['password'],
-        server = share_link['server'],
-        port = share_link['port'],
-        allowInsecure = share_link['allowInsecure'] if share_link.get('allowInsecure') else "0",
-        sni = share_link['sni'],
-        skip_cert_verify = "&skip-cert-verify=1" if share_link.get('skip-cert-verify') else "",
-        name = quote(share_link['name'], 'utf-8')
-        )
+        trojan_info = {
+            "password": share_link['password'],
+            "server": share_link['server'],
+            "port": share_link['port'],
+            "allowInsecure": share_link.get('allowInsecure', '1'),
+            "sni": share_link.get('sni', ''),
+            "skip_cert_verify": '1' if share_link.get('skip-cert-verify') else "",
+            "type": share_link.get('network', 'tcp'),
+            "fp": share_link.get('client-fingerprint', ''),
+            "alpn": quote(','.join(share_link['alpn']), 'utf-8'),
+            "name": quote(share_link['name'], 'utf-8')
+        }
+        if trojan_info['type'] == 'grpc':
+            if share_link.get('grpc-opts').get('grpc-service-name') != '/' :
+                trojan_info["serviceName"] = unquote(share_link.get('grpc-opts').get('grpc-service-name'))
+            else:
+                trojan_info["serviceName"] = ''
+            link = "trojan://{password}@{server}:{port}?allowInsecure={allowInsecure}&sni={sni}&skip_cert_verify={skip_cert_verify}&type={type}&serviceName={serviceName}&fp={fp}&alpn={alpn}#{name}".format(**trojan_info)
+        if trojan_info['type'] == 'ws':
+            if share_link.get('ws-opts'):
+                trojan_info["path"] = quote(share_link['ws-opts'].get('path', ''), 'utf-8')
+                trojan_info["host"] = share_link['ws-opts']['headers'].get('Host', '')
+            else:
+                trojan_info["path"] = ''
+                trojan_info["host"] = trojan_info["sni"]
+            link = "trojan://{password}@{server}:{port}?allowInsecure={allowInsecure}&sni={sni}&skip_cert_verify={skip_cert_verify}&type={type}&host={host}&path={path}&fp={fp}&alpn={alpn}#{name}".format(**trojan_info)
+        if trojan_info['type'] == 'tcp':
+            link = "trojan://{password}@{server}:{port}?allowInsecure={allowInsecure}&sni={sni}&skip_cert_verify={skip_cert_verify}&type={type}&fp={fp}&alpn={alpn}#{name}".format(**trojan_info)
     elif share_link['type'] == 'vless':
         vless_info = {
             "uuid": share_link['uuid'],
@@ -75,7 +103,7 @@ def clash2v2ray(share_link):
             "type": share_link.get('network', 'tcp'),
             "flow": share_link.get('flow', ''),
             "name": quote(share_link['name'], 'utf-8')
-        }   
+        }
         if vless_info['type'] == 'ws':
             vless_info["security"] = 'tls'
             vless_info["path"] = quote(share_link['ws-opts']['path'], 'utf-8')
@@ -104,4 +132,18 @@ def clash2v2ray(share_link):
                 vless_info["security"] = 'tls'
                 link = "vless://{uuid}@{server}:{port}?encryption=none&security={security}&sni={sni}&serverName={sni}&type={type}&fp={fp}&flow={flow}#{name}".format(**vless_info)
         # TODO
+    elif share_link['type'] == 'tuic':
+        link = "tuic://{uuid}:{password}@{server}:{port}?alpn={alpn}&allow_insecure={allowInsecure}&disable_sni={disable_sni}&sni={sni}&udp_relay_mode={udp_relay_mode}&congestion_control={control}#{name}".format(
+        uuid = share_link['uuid'],
+        password = share_link['password'],
+        server = share_link['server'],
+        port = share_link['port'],
+        alpn = quote(','.join(share_link['alpn']), 'utf-8'),
+        allowInsecure = share_link.get('allowInsecure', '1'),
+        disable_sni = '0' if share_link.get('disable-sni', '') == False else '1',
+        sni = share_link.get('sni', ''),
+        udp_relay_mode = share_link.get('udp-relay-mode', 'native'),
+        control = share_link.get('congestion-controller', 'bbr'),
+        name = share_link['name'].encode('utf-8', 'surrogatepass').decode('utf-8')
+        )
     return link
