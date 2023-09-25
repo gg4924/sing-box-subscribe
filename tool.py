976038b9c464c75a0a53b82d99a55eeeba32522b
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-09-25 11:21:35 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -279,10 +279,11 @@ def getResponse(url):
     response = None
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'
+        #'User-Agent': 'clash.meta'
     }
     try:
-        response = requests.get(url, headers=headers, timeout=5000)
-        if response.status_code == 200:
+        response = requests.get(url,headers=headers,timeout=5000)
+        if response.status_code==200:
             return response
         else:
             return None
