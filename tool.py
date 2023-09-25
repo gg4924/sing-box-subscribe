b22f6282ba048760ff0b12b38a2f7e93c53560d1
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-09-25 10:54:37 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -275,10 +275,10 @@ def prefixStr(nodelist,prestr):
         node['name'] = prestr+node['name'].strip()
     return nodelist
 
-def getResponse(url, custom_user_agent=None):
+def getResponse(url):
     response = None
     headers = {
-        'User-Agent': custom_user_agent if custom_user_agent else 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'
     }
     try:
         response = requests.get(url, headers=headers, timeout=5000)
