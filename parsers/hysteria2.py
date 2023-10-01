13902f93641056c172ddad9dfc9b8322065bfc95
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-10-01 12:39:01 +0800

    fix hysteria2 tls

--- parsers/hysteria2.py
+++ parsers/hysteria2.py
@@ -13,21 +13,22 @@ def parse(data):
         'type': 'hysteria2',
         'server': re.sub(r"\[|\]", "", server_info.netloc.split("@")[1].rsplit(":", 1)[0]),
         'server_port': int(server_info.netloc.rsplit(":", 1)[1].split(",")[0]),
-        'obfs': {
-            'type': netquery.get('obfs', ''),
-            'password': netquery.get('obfs-password', ''),
-        },
         "password": server_info.netloc.split("@")[0].rsplit(":", 1)[-1],
         'tls': {
             'enabled': True,
             'server_name': netquery.get('sni', re.sub(r"\[|\]", "", server_info.netloc.split("@")[1].rsplit(":", 1)[0]))
         }
     }
-    node['tls']['alpn'] = netquery.get('alpn').strip('{}').split(',') if netquery.get('alpn') != '' else ["h3"]
+    if netquery.get('insecure') and netquery['insecure'] == '1' or netquery.get('allowInsecure') and netquery['allowInsecure'] == '1':
+        node['tls']['insecure'] = True
+    node['tls']['alpn'] = (netquery.get('alpn') or "h3").strip('{}').split(',')
+    if netquery.get('obfs'):
+        node['obfs'] = {
+            'type': netquery['obfs'],
+            'password': netquery['obfs-password'],
+        }
     if netquery.get('upmbps'):
         node['tls']['up_mbps'] = int(netquery.get('upmbps'))
     if netquery.get('downmbps'):
         node['tls']['down_mbps'] = int(netquery.get('downmbps'))
-    if netquery.get('insecure') and netquery['insecure'] == '1' or netquery.get('allowInsecure') and netquery['allowInsecure'] == '1':
-        node['tls']['insecure'] = True
     return (node)
