5d7a6442ffaec6774fc760c13716afdbe84f91af
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-10-09 15:18:55 +0800

    f*ck

--- parsers/vmess.py
+++ parsers/vmess.py
@@ -1,11 +1,50 @@
 import tool,json,re,urllib,sys
+from urllib.parse import urlparse, parse_qs, unquote
 def parse(data):
     info = data[8:]
     if not info or info.isspace():
         return None
-    #print(info)
     try:
-        proxy_str = tool.b64Decode(info).decode('utf-8')
+        if info.find('?') > -1: #fuck奇葩的URI格式
+            server_info = urlparse(info)
+            netquery = dict(
+                (k, v if len(v) > 1 else v[0])
+                for k, v in parse_qs(server_info.query).items()
+            )
+            _path = tool.b64Decode(server_info.path).decode('utf-8').split("@")
+            node = {
+                'tag': netquery.get('remarks', tool.genName()+'_vmess'),
+                'type': 'vmess',
+                'server': _path[1].split(":")[0],
+                'server_port': int(_path[1].split(":")[1]),
+                'uuid': _path[0].split(":")[1],
+                'security': _path[0].split(":")[0],
+                'alter_Id': int(netquery.get('alterId')),
+                'packet_encoding': 'xudp'
+            }
+            if netquery.get('tls') and netquery['tls'] != '':
+                node['tls']={
+                    'enabled': True,
+                    'insecure': True,
+                    'server_name': netquery.get('peer', '')
+                }
+                if netquery.get('sni'):
+                    node['tls']['server_name'] = netquery['sni']
+                    node['tls']['utls'] = {
+                        'enabled': True,
+                        'fingerprint': netquery.get('fp', '')
+                    }
+            if netquery.get('obfs') == 'websocket':
+                node['transport'] = {
+                    'type': 'ws',
+                    'path': netquery.get('path', '').rsplit("?")[0],
+                    'headers': {
+                        'Host': json.loads(netquery.get('obfsParam')).get('Host', '') if netquery.get('obfsParam') else ''
+                    }
+                }
+            return node
+        else:
+            proxy_str = tool.b64Decode(info).decode('utf-8')
     except:
         print(info)
         return None
@@ -47,7 +86,7 @@ def parse(data):
                 'type':'ws',
                 'path':item.get('path', '').rsplit("?")[0],
                 'headers': {
-                'Host': item.get('host', '')
+                    'Host': item.get('host', '')
                 }
             }
         if item['net'] == 'quic':
