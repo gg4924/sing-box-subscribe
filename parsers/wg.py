aa116193fd78c6a91af95e5ccb82ddfd0c1ac4db
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-10-11 11:09:21 +0800

    fix wg.py

--- parsers/wg.py
+++ parsers/wg.py
@@ -4,7 +4,7 @@ def parse(data):
     info = data[:]
     server_info = urlparse(info)
     netquery = dict(
-        (k, v if len(v) > 1 else v[0])
+        (k, v.replace(' ', '+') if len(v) > 1 else v[0].replace(' ', '+'))
         for k, v in parse_qs(server_info.query).items()
     )
     _reserved = netquery.get('reserved').split(",")
@@ -14,14 +14,14 @@ def parse(data):
         'server': re.sub(r"\[|\]", "", server_info.netloc.rsplit(":", 1)[0]),
         'server_port': int(server_info.netloc.rsplit(":", 1)[1]),
         'local_address': [
-            netquery.get('ip'),
+            netquery.get('ip').split(",", 1)[0]+"/32"
         ],
         'private_key': netquery.get('privateKey'),
         'peer_public_key': netquery.get('publicKey'),
-        'pre_shared_key': netquery.get('presharedKey', ''),
-        'reserved': [int(_reserved[0]),int(_reserved[1]),int(_reserved[2])],
-        'mtu': int(netquery.get('mtu'))
+        'reserved': [int(_reserved[0]),int(_reserved[1]),int(_reserved[2])]
     }
-    if netquery.get('ipv6'):
-        node['local_address'].append(netquery.get('ipv6'))
+    if netquery.get('ip').split(",", 1)[1]:
+        node['local_address'].append(netquery.get('ip').split(",", 1)[1]+"/128")
+    if netquery.get('presharedKey'):
+        node['pre_shared_key'] = netquery['presharedKey']
     return (node)
