7bd1f8aac5869a3e47be5b9522dbbc3a778cf904
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-10-11 23:19:51 +0800

    Update http.py

--- parsers/http.py
+++ parsers/http.py
@@ -9,7 +9,7 @@ def parse(data):
         for k, v in parse_qs(server_info.query).items()
     )
     node = {
-        'tag': unquote(server_info.fragment),
+        'tag': unquote(server_info.fragment) or tool.genName()+'_http',
         'type': 'http',
         'server': re.sub(r"\[|\]", "", _netloc[1].rsplit(":", 1)[0]),
         'server_port': int(_netloc[1].rsplit(":", 1)[1]),
