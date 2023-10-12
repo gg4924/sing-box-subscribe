57735d5ed679e5b6291fb0b321fabb2551bf1fc3
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-10-12 10:42:41 +0800

    Update and rename http.py to https.py

--- parsers/https.py
+++ parsers/https.py
@@ -3,24 +3,26 @@ from urllib.parse import urlparse, parse_qs, unquote
 def parse(data):
     info = data[:]
     server_info = urlparse(info)
-    _netloc = (tool.b64Decode(server_info.netloc)).decode().rsplit("@", 1)
+    try:
+        remark = (tool.b64Decode(server_info.netloc)).decode().rsplit("/#", 1)
+    except UnicodeDecodeError:
+        remark = (tool.b64Decode(server_info.netloc+server_info.path)).decode().rsplit("/#", 1)
+    _netloc = remark[0].rsplit("@", 1)
     netquery = dict(
         (k, v if len(v) > 1 else v[0])
         for k, v in parse_qs(server_info.query).items()
     )
     node = {
-        'tag': unquote(server_info.fragment) or tool.genName()+'_http',
+        'tag': unquote(remark[1]) if len(remark) > 1 else tool.genName() + '_http',
         'type': 'http',
         'server': re.sub(r"\[|\]", "", _netloc[1].rsplit(":", 1)[0]),
         'server_port': int(_netloc[1].rsplit(":", 1)[1]),
         'username': _netloc[0].split(":")[0],
         'password': _netloc[0].split(":")[1],
-        'path': server_info.path
-    }
-    if netquery.get('tls') == '1':
-        node['tls'] = {
+        'tls': {
             'enabled': True
         }
+    }
     if netquery.get('insecure') and netquery['insecure'] == '1' :
         node['tls']['insecure'] = True
     return (node)
