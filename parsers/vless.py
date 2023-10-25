c8cf819047eea4677319ae8a852aa4f65f8b3465
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-10-25 16:34:34 +0800

    fix vless+ws

--- parsers/vless.py
+++ parsers/vless.py
@@ -48,6 +48,9 @@ def parse(data):
                     "Host": netquery.get('sni', netquery.get('host', ''))
                 }
             }
+            if '?ed' in netquery.get('path'):
+                node['transport']['early_data_header_name'] = 'Sec-WebSocket-Protocol'
+                node['transport']['max_early_data'] = int(netquery.get('path').rsplit("?ed=")[1])
         if netquery['type'] == 'grpc':
             node['transport'] = {
                 'type':'grpc',
