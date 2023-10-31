04417b8aa0f1bfd810a6c65123999b97fbc12d5e
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-10-31 13:01:16 +0800

    fix socks.py

--- parsers/socks.py
+++ parsers/socks.py
@@ -3,19 +3,20 @@ from urllib.parse import urlparse, parse_qs, unquote
 def parse(data):
     info = data[:]
     server_info = urlparse(info)
-    _netloc = (tool.b64Decode(server_info.netloc)).decode().split("@")
-    netquery = dict(
-        (k, v if len(v) > 1 else v[0])
-        for k, v in parse_qs(server_info.query).items()
-    )
     node = {
-        'tag': netquery.get('remarks'),
+        'tag': unquote(server_info.fragment)  or tool.genName()+'socks',
         'type': 'socks',
-        'server': re.sub(r"\[|\]", "", _netloc[1].rsplit(":", 1)[0]),
-        'server_port': int(_netloc[1].rsplit(":", 1)[1]),
         "version": "5",
-        'username': _netloc[0].split(":")[0],
-        'password': _netloc[0].split(":")[1],
         'udp_over_tcp': {}
     }
+    netloc = (tool.b64Decode(server_info.netloc)).decode()
+    if '@' in netloc:
+        _netloc = netloc.split("@")
+        node['server'] = re.sub(r"\[|\]", "", _netloc[1].rsplit(":", 1)[0])
+        node['server_port'] = int(_netloc[1].rsplit(":", 1)[1])
+        node['username'] = _netloc[0].split(":")[0]
+        node['password'] = _netloc[0].split(":")[1]
+    else:
+        node['server'] = re.sub(r"\[|\]", "", netloc.rsplit(":", 1)[0])
+        node['server_port'] = int(netloc.rsplit(":", 1)[1])
     return (node)
