28f07a09abc7e09e52c893d61c7d4065d31e5dba
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-11-04 15:28:50 +0800

    Update vless.py

--- parsers/vless.py
+++ parsers/vless.py
@@ -3,7 +3,12 @@ from urllib.parse import urlparse, parse_qs, unquote
 def parse(data):
     info = data[:]
     server_info = urlparse(info)
-    _netloc = server_info.netloc.split("@")
+    print (server_info)
+    try:
+        netloc = tool.b64Decode(server_info.netloc).decode('utf-8')
+    except:
+        netloc = server_info.netloc
+    _netloc = netloc.split("@")
     netquery = dict(
         (k, v if len(v) > 1 else v[0])
         for k, v in parse_qs(server_info.query).items()
@@ -13,7 +18,7 @@ def parse(data):
         'type': 'vless',
         'server': re.sub(r"\[|\]", "", _netloc[1].rsplit(":", 1)[0]),
         'server_port': int(_netloc[1].rsplit(":", 1)[1]),
-        'uuid': _netloc[0],
+        'uuid': _netloc[0].split(':', 1)[-1],
         'packet_encoding': netquery.get('packetEncoding', 'xudp')
     }
     if netquery.get('flow'):
