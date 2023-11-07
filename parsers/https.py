578c860496a6e597757776a52ff6c2387926f535
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-11-07 23:29:57 +0800

    Update https.py

--- parsers/https.py
+++ parsers/https.py
@@ -3,10 +3,11 @@ from urllib.parse import urlparse, parse_qs, unquote
 def parse(data):
     info = data[:]
     server_info = urlparse(info)
+    print (server_info)
     try:
-        remark = (tool.b64Decode(server_info.netloc)).decode().rsplit("/#", 1)
+        remark = tool.b64Decode(server_info.netloc+server_info.path).decode().rsplit("/#", 1)
     except UnicodeDecodeError:
-        remark = (tool.b64Decode(server_info.netloc+server_info.path)).decode().rsplit("/#", 1)
+        remark = (server_info.netloc).rsplit("/#", 1)
     _netloc = remark[0].rsplit("@", 1)
     node = {
         'tag': unquote(remark[1]) if len(remark) > 1 else tool.genName() + '_http',
