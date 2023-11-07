a05fbccc2fc7af61279103c10827b9184ebc156d
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-11-07 11:08:07 +0800

    fix bug

--- parsers/https.py
+++ parsers/https.py
@@ -3,26 +3,32 @@ from urllib.parse import urlparse, parse_qs, unquote
 def parse(data):
     info = data[:]
     server_info = urlparse(info)
+    '''
     try:
         remark = (tool.b64Decode(server_info.netloc)).decode().rsplit("/#", 1)
     except UnicodeDecodeError:
         remark = (tool.b64Decode(server_info.netloc+server_info.path)).decode().rsplit("/#", 1)
+    remark = unquote(remark[1]) if len(remark) > 1 else tool.genName() + '_http'
     _netloc = remark[0].rsplit("@", 1)
-    netquery = dict(
-        (k, v if len(v) > 1 else v[0])
-        for k, v in parse_qs(server_info.query).items()
-    )
+    '''
+    remark = server_info.fragment
+    netloc = (tool.b64Decode(server_info.netloc)).decode()
+    if '@' in netloc:
+        _netloc = netloc.rsplit("@", 1)
+        server_port = _netloc[1]
+    else:
+       server_port = netloc
     node = {
-        'tag': unquote(remark[1]) if len(remark) > 1 else tool.genName() + '_http',
+        'tag': remark,
         'type': 'http',
-        'server': re.sub(r"\[|\]", "", _netloc[1].rsplit(":", 1)[0]),
-        'server_port': int(_netloc[1].rsplit(":", 1)[1]),
-        'username': _netloc[0].split(":")[0],
-        'password': _netloc[0].split(":")[1],
+        'server': re.sub(r"\[|\]", "", server_port.rsplit(":", 1)[0]),
+        'server_port': int(server_port.rsplit(":", 1)[1]),
         'tls': {
-            'enabled': True
+            'enabled': True,
+            'insecure': True
         }
     }
-    if netquery.get('insecure') and netquery['insecure'] == '1' :
-        node['tls']['insecure'] = True
+    if '@' in netloc:
+        node['username'] = _netloc[0].split(":")[0]
+        node['password'] = _netloc[0].split(":")[1]
     return (node)
