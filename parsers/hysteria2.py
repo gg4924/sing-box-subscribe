792eee111b052322fb62dc165f43cc9f31e70648
Dorae <86833913+Toperlock@users.noreply.github.com>
2023-12-18 14:09:49 +0800

    Update hysteria2.py

--- parsers/hysteria2.py
+++ parsers/hysteria2.py
@@ -26,7 +26,7 @@ def parse(data):
     if netquery.get('insecure') and netquery['insecure'] == '1' or netquery.get('allowInsecure') and netquery['allowInsecure'] == '1':
         node['tls']['insecure'] = True
     if node['tls']['server_name'] == '':
-        del ['tls']['server_name']
+        del node['tls']['server_name']
         node['tls']['insecure'] = True
     node['tls']['alpn'] = (netquery.get('alpn') or "h3").strip('{}').split(',')
     if netquery.get('obfs', '') not in ['none', '']:
