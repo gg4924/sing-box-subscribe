1a7e078c3fc5f2e8ca527405884811d3c81ce368
Dorae <86833913+Toperlock@users.noreply.github.com>
2024-01-11 13:38:03 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -108,7 +108,7 @@ def edit_temp_json():
 
 @app.route('/config/<path:url>', methods=['GET'])
 def config(url):
-    if 'gitlab.com' in url or 'sing-box-subscribe.vercel.app' in url:
+    if any(substring in url for substring in ['gitlab.com', 'sing-box-subscribe.vercel.app', 'https://xxxxxxx', 'URL_LINK', 'URLLINK', '订阅', '你的', 'SUB']):
         return None
     # temp_json_data_str = os.environ['TEMP_JSON_DATA']
     # temp_json_data = json.loads(temp_json_data_str)
