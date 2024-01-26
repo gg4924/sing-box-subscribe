f67763002a1a16daf4a4523dc14b1ba2977a328b
Toperlock <861705708@qq.com>
2024-01-26 16:49:58 +0800

    compatible with other `ssr` format conversion

--- parsers/ssr.py
+++ parsers/ssr.py
@@ -3,10 +3,18 @@ def parse(data):
     info = data[6:]
     if not info or info.isspace():
         return None
-    proxy_str = tool.urlDecode(info).decode('utf-8')
+    try:
+        proxy_str = tool.urlDecode(info).decode('utf-8')
+    except:
+        proxy_str = info
     parts = proxy_str.split(':')
-    if len(parts) != 6:
-        return None
+    if len(parts) == 5: #fuck
+        next_part, _, proxy_str = proxy_str.partition('=')
+        parts.append(proxy_str)
+        for ssr_obfs in ['plain', 'http_simple', 'http_post', 'random_head', 'tls1.2_ticket_auth']:
+            if ssr_obfs in parts[4]:
+                parts[5] = parts[4].split(ssr_obfs)[-1]
+                parts[4] = ssr_obfs
     node = {
         'tag':None,
         'type':'shadowsocksr',
@@ -17,8 +25,8 @@ def parse(data):
         'obfs': parts[4]
     }
     password_params = parts[5].split('/?')
-    node['password'] = tool.urlDecode(password_params[0]).decode('utf-8')
-    params = password_params[1].split('&')
+    node['password'] = tool.urlDecode(password_params[0].split('remarks')[0]).decode('utf-8')
+    params = password_params[-1].split(password_params[0].split('remarks')[0])[-1].split('&')
     pdict = {'obfsparam':'obfs_param','protoparam':'protocol_param','remarks':'tag'}
     for p in params:
         key_value = p.split('=')
